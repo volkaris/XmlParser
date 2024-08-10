c2922fe9af13da3aec32abc64163401ea93061c9
Ilya <zaxsad109@gmail.com>
2024-08-10 16:03:03 +0300

    migrations fixed

---
+++ Migrations/20240810125131_initialSetup.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace XmlParser.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialSetup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OrderItems",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    orderId = table.Column<int>(type: "integer", nullable: false),
+                    productId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderItems", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    regDate = table.Column<string>(type: "text", nullable: false),
+                    Sum = table.Column<double>(type: "double precision", nullable: false),
+                    user_id = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    quantity = table.Column<int>(type: "integer", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    price = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    fio = table.Column<string>(type: "text", nullable: false),
+                    email = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderItems");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-
 using XmlParser.ChainOfResponsibility;
 using XmlParser.DbContexts;
 using XmlParser.OperationResults;
@@ -19,52 +18,55 @@ internal class Program
 {
     private static void Main()
     {
-        var serviceCollection = new ServiceCollection();
+        var builder = WebApplication.CreateBuilder();
 
-        var configuration = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json", false, true)
-            .Build();
+        builder.Services.AddDbContext<OrdersDbContext>(
+            options =>
+                options.UseNpgsql(
+                    builder.Configuration.GetConnectionString("DefaultConnection")));
+        builder.Services.AddScoped<IUserService, UserService>();
+        builder.Services.AddScoped<IUserRepository, UserRepository>();
 
+        builder.Services.AddScoped<IOrderService, OrderService>();
+        builder.Services.AddScoped<IOrderRepository, OrderRepository>();
 
-        serviceCollection.AddScoped<IUserService, UserService>();
-        serviceCollection.AddScoped<IUserRepository, UserRepository>();
+        builder.Services.AddScoped<IOrderItemsService, OrderItemsService>();
+        builder.Services.AddScoped<IOrderItemRepository, OrderItemsRepository>();
 
-        serviceCollection.AddScoped<IOrderService, OrderService>();
-        serviceCollection.AddScoped<IOrderRepository, OrderRepository>();
+        builder.Services.AddScoped<IProductService, ProductService>();
+        builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
-        serviceCollection.AddScoped<IOrderItemsService, OrderItemsService>();
-        serviceCollection.AddScoped<IOrderItemRepository, OrderItemsRepository>();
+        var app = builder.Build();
 
-        serviceCollection.AddScoped<IProductService, ProductService>();
-        serviceCollection.AddScoped<IProductRepository, ProductRepository>();
-
-        serviceCollection.AddDbContext<OrdersDbContext>(
-            opt => opt.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
-
-        var serviceProvider = serviceCollection.BuildServiceProvider();
-        
-        var parser = new Parser("C:\\Users\\Ilya\\RiderProjects\\XmlParser\\XmlParser\\example.xml");
-        
-        var order = parser.OrderDto;
-        
-        var orderService = serviceProvider.GetRequiredService<IOrderService>();
-        var userService = serviceProvider.GetRequiredService<IUserService>();
-        var orderItemsService = serviceProvider.GetRequiredService<IOrderItemsService>();
-        var productService = serviceProvider.GetRequiredService<IProductService>();
-
-        var productChainLink = new ProductsChainLink(productService);
-        var orderItemChainLink = new OrderItemsChainLink(orderItemsService);
-        var userChainLink = new UsersChainLink(userService);
-        var orderChainLink = new OrderChainLink(orderService);
-
-        productChainLink.AddNext(userChainLink).AddNext(orderChainLink).AddNext(orderItemChainLink);
-
-        var parsingResult = productChainLink.ProcessOrder(order);
-
-        if (parsingResult is OperationExecutionResult.Unsuccess unsuccess)
+        using (var scope = app.Services.CreateScope())
         {
-            Console.WriteLine(unsuccess.FailReason);
+            var dbContext = scope.ServiceProvider.GetRequiredService<OrdersDbContext>();
+            dbContext.Database.Migrate();
+        }
+
+        using (var scope = app.Services.CreateScope())
+        {
+            var serviceProvider = scope.ServiceProvider;
+
+            var parser = new Parser("C:\\Users\\Ilya\\RiderProjects\\XmlParser\\XmlParser\\example.xml");
+
+            var order = parser.OrderDto;
+
+            var orderService = serviceProvider.GetRequiredService<IOrderService>();
+            var userService = serviceProvider.GetRequiredService<IUserService>();
+            var orderItemsService = serviceProvider.GetRequiredService<IOrderItemsService>();
+            var productService = serviceProvider.GetRequiredService<IProductService>();
+
+            var productChainLink = new ProductsChainLink(productService);
+            var orderItemChainLink = new OrderItemsChainLink(orderItemsService);
+            var userChainLink = new UsersChainLink(userService);
+            var orderChainLink = new OrderChainLink(orderService);
+
+            productChainLink.AddNext(userChainLink).AddNext(orderChainLink).AddNext(orderItemChainLink);
+
+            var parsingResult = productChainLink.ProcessOrder(order);
+
+            if (parsingResult is OperationExecutionResult.Unsuccess unsuccess) Console.WriteLine(unsuccess.FailReason);
         }
     }
 }
